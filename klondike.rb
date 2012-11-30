7f08d488314b45fc16a72e5872f2467ca259b0de
jcreed <jcreed63@verizon.net>
2012-11-30 12:13:34 -0800

    Computer play working

--- klondike.rb
+++ klondike.rb
@@ -14,13 +14,13 @@ class Cards
 
 	attr_accessor :deck
 
-	def new_game
-		get_deck
-		shuffle
-	end
+	# def new_game
+	# 	get_deck
+	# 	shuffle
+	# end
 
 	# create a stock deck
-	def get_deck
+	def self.get_deck
 		(0..3).each do |suit|
 			(0..12).each do |value|
 				@@deck[@@deck.length] = @@values.keys[value] + @@suits.keys[suit]
@@ -28,15 +28,25 @@ class Cards
 		end
 	end
 
-	def shuffle
-		@@deck.shuffle!
+	def self.shuffle
+		10.times {@@deck.shuffle!}
+		@@deck
 	end
 end
 
+class Numeric
+	def percent_of(n)
+		self.to_f / n.to_f * 100
+	end
+end
 
 class Klondike < Cards
 	FOUNDATION_PILES = 4
 	TABLEAU_PILES = 7
+	SHOW = true
+	SILENT = false
+
+	#validates_inclusion_of :from, :in => 1..7, "can only be between 1 and 7."
 
 	def initialize
 		@stock_pile = Array.new			#left over for play
@@ -59,24 +69,44 @@ class Klondike < Cards
 	end
 
 	def new_game
-		@stock_pile = super()		# create shuffled cards
-		deal_cards							# setup cards
+		#@stock_pile = super()		# create shuffled cards
+		Cards.get_deck								# get new deck
+		@stock_pile = Cards.shuffle  		# shuffle cards
+		deal_cards												# setup cards
 	 	@stock_count = @stock_pile.count  
-	 	start 									# start game
+	 	user = ''
+		until user == 'C' || user == "P" do
+			print "(P)layer or (C)omputer -> "
+	 		user = gets.chomp.upcase.slice(0) 
+		end
+	 	@verbose =  user == "P" ? SHOW : SILENT
+	 	start 															# start game
 	end
 
 	# play game
 	def start
-		 catch (:quit) do
+		catch (:quit) do
 		 	loop do
-		 		flip_card 				# get new card
-				show_tableau			# show tableau cards
-				show_foundation		# show foundation cards
-				play_game					# play game
-				reset_stock if stock_finished?				# test deck stock condition
+		 		flip_card if !current_card_exist?		# get new card from deck
+		 		if verbose?
+					show_tableau 	 			# display tableau cards in play
+					show_foundation				# display foundation cards played
+					player_game								# play game play
+				else
+					computer_game 						# computer play
+					puts "flip"
+				end
+				reset_stock if stock_finished?					# test deck stock condition
 			end
-		 end
-		puts "Game over try again!"
+		end
+		show_tableau
+		show_foundation
+		if gamewon? 
+			puts "We have a winner!" 
+		else
+			puts "Deck is not playable - game over"
+		end
+		
 	end
 
 	# setup the 7 tableau piles to be played turning over the first card
@@ -90,18 +120,26 @@ class Klondike < Cards
 	end
 
 	def stock_finished?
-		(@stock_pile.count == @cards_played + @discard_pile.count) || @stock_pile.count == 0
+		#puts "played #{@cards_played} dicarded #{@discard_pile.count}"
+		(@stock_count == @cards_played + @discard_pile.count) || @stock_pile.count == 0
 	end
 
 	def reset_stock
-	  @stock_useless += 1 if @stock_count = @discard_pile.count
+		if @stock_count == @discard_pile.count
+	  	@stock_useless += 1 
+	  else
+	  	@stock_useless = 0
+	  end
 		@cards_played = 0
 		@stock_pile = @discard_pile
 	 	@stock_count = @stock_pile.count  
 		@discard_pile = []
-		puts "  *Discard pile is full lets replay the them* - There are #{@stock_count} cards left"
+		puts "  *Discard pile is full lets replay the them* - There are #{@stock_count} cards left" #if verbose?
 	end
 
+	def verbose?
+		@verbose
+	end
 
 	# get cards type A,K,Q etc
 	def card_type(card)
@@ -125,74 +163,175 @@ class Klondike < Cards
 
 	# flip a card to be played
 	def flip_card
-		@cur_card = @stock_pile.shift if @cur_card.empty?		# get new card from deck
+		@cur_card = @stock_pile.shift 
 	end
 
 	# try to move a tableau to another tableau  pile
 	def move_pile
-			puts "Move pile (1-7)" 
-			m = gets.chomp.slice(0)
-			puts "To pile (1-7)" 
-			t = gets.chomp.slice(0)
-			if ok_move_pile_to_pile_for_tableau?(m, t)	
-				move_tableau_pile(m, t) 
-				puts "Pile moved"
+			from, to = 0, 0
+			until (1..7).include?(from.to_i) do
+				print "Move pile (1-7) -> " 
+				from = gets.chomp.slice(0)
+			end
+			
+			until (1..7).include?(to.to_i) do
+				print "To pile (1-7) -> " 
+				to = gets.chomp.slice(0)
+			end
+			if !@tableau[from.to_s+"U"].empty? && ok_move_pile_to_pile_for_tableau?(from, to)
+				move_tableau_pile(from, to) 
+				puts "Pile moved" if verbose?
 			else
-				puts "Piles don't match up"
+				puts "Piles don't match up" if verbose?
 			end
 	end
 
 	# get users play
 	def get_card_play
-		print "Use on (F)oundaton or Pile (1-7) ->"	# main play
-		gets.chomp.upcase.slice(0)
+		p = ''
+		until (1..7).include?(p.to_i) || p == "F" do
+			print "Use on (F)oundaton or Pile (1-7) -> "	# main play
+			p = gets.chomp.upcase.slice(0)
+		end
+		p
 	end
 
 	# get users play on discard
-	def get_discard_play
-		print "Use on Pile (1-7) ->"	# main play
-		gets.chomp.upcase.slice(0)
+	def get_play
+		p = ''
+		until (1..7).include?(p.to_i) do
+			print "Use on Pile (1-7) -> "	# main play
+			p = gets.chomp.upcase.slice(0)
+		end
+		p
 	end
 
 	# show game information
-	def play_game
+	def player_game
+		throw :quit if gameover? 
 		choice = get_players_move
-		if choice == "Q" || gameover?
+		if choice == "Q" 
 			throw :quit 
 		elsif choice == "M" 	#Move a pile
 			move_pile 				
 		elsif choice == "U" && card_type(@discard_pile.last) == 'K'	#Move discard (King) to tableau
-			discard_play = get_discard_play 
-	  	move_to_tableau(@discard_pile.last, discard_play)	if card_ok_for_tableau?(@discard_pile.last, discard_play) # tableau move
+			use_pile = get_play 
+	  	move_to_tableau(@discard_pile.last, use_pile)	if card_ok_for_tableau?(@discard_pile.last, use_pile) # tableau move
 		elsif choice == "D" 	#Discard
 	  	@discard_pile << @cur_card
 		  @cur_card = ""
 		elsif choice == "P" 	#Make a Play	
-			play = get_card_play	
-			if play == "F" 
-				print "(C)urrent card or (T)ableau card ->"
-				m = gets.upcase.chomp
-				move_curcard_to_foundation	if m == "C" 	# current card to foundation 
-				move_tableau_to_foundation	if m == "T" 	# a tableau card to foundation 
-			elsif (1..7).include?(play.to_i)
-				if card_ok_for_tableau?(@cur_card, play) # tableau move
-					move_to_tableau(@cur_card, play)	
+			where = get_card_play	
+			if where == "F" 
+				move = ''
+				until move == 'C' || move == 'T' do
+					print "(C)urrent card or (T)ableau card ->"
+					move = gets.upcase.chomp
+				end
+				if move == "C" 	
+					move_curcard_to_foundation		# current card to foundation 
+				else
+					use_pile = get_play
+					move_tableau_to_foundation(use_pile) if @tableau[use_pile.to_s+"U"].last	 	# a tableau card to foundation 
+				end
+			elsif (1..7).include?(where.to_i)
+				if card_ok_for_tableau?(@cur_card, where) # tableau move
+					move_to_tableau(@cur_card, where)	
 					set_card_was_played
 				end
 			else
-				puts "Invalid option"
+				puts "Invalid option" 
 			end
 		else
-			puts "Invalid option"
+			puts "Invalid option" 
+		end
+	end
+
+	def computer_game
+		throw :quit if computer_gameover? 
+		test_move_tableau_aces			# check for tableau Ace to move to foundation
+		test_pile_to_pile							# check for a tableau pile to pile move
+		test_pile_to_foundation					# check for a tableau card to foundation move
+		test_current_card
+		test_pile_to_foundation					# check for a tableau card to foundation move
+		test_stock
+	end	
+
+	def test_current_card
+		current_card = @cur_card
+		test_current_card_to_tableau if current_card_exist?			# check for current card to tableau move
+		move_curcard_to_foundation  if current_card_exist?					# current card to foundation 
+		if current_card_exist? && @cur_card.eql?(current_card)
+			puts "Discarded Card"
+	  	@discard_pile << @cur_card
+		  @cur_card = ""
+		end
+	end
+
+	def test_move_tableau_aces
+		(1..7).each do |pile|
+			move_tableau_to_foundation(pile) if tableau_piles_have_ace && @tableau[pile.to_s+"U"].last
+		end
+	end
+
+	def test_current_card_to_tableau
+		(1..7).each do |where|
+			if current_card_exist? && card_ok_for_tableau?(@cur_card, where) # tableau move
+				move_to_tableau(@cur_card, where)	
+				set_card_was_played
+			end
+		end
+	end
+
+	def current_card_exist?
+		!@cur_card.eql?(nil) && !@cur_card.empty?
+	end
+
+	def tableau_piles_have_ace
+		get_bottom_tableau_cards.each.map.include?(true) {|t| card_type(t).eql?('A')}
+	end
+
+	def test_pile_to_pile
+		move_a_pile_to_pile if (current_and_pile_has_same_play(get_bottom_tableau_cards) && !almost_thru_unused_stock?) ||
+													!current_and_pile_has_same_play(get_bottom_tableau_cards)
+	end
+
+	def test_pile_to_foundation
+		bottom = get_bottom_tableau_cards
+		(1..7).each do |pile|
+			move_tableau_to_foundation(pile) if @tableau[pile.to_s+"U"].last
+		end
+		test_pile_to_foundation if !get_bottom_tableau_cards.eql?(bottom)
+	end
+
+	def almost_thru_unused_stock?
+		@cards_played.eql?(0) && @stock_pile.count.percent_of(@stock_count) > 75
+	end
+
+	def current_and_pile_has_same_play(cards)
+		cards.each.map.include?(true) {|card|	(card_suit_color(card) == card_suit_color(@cur_card)) && (card_type(card) == card_type(@cur_card))}
+	end
+
+	def get_bottom_tableau_cards
+		@tableau.each.map {|k,v| v.last if v.last}
+	end
+
+	def move_a_pile_to_pile
+		(1..7).each do |p1|
+			(1..7).each	do |p2|
+				if !p1.eql?(p2) && !@tableau[p1.to_s+"U"].empty? && ok_move_pile_to_pile_for_tableau?(p1, p2) 
+					move_tableau_pile(p1, p2) 
+				end
+			end
 		end
 	end
 
 	def get_players_move
-		puts "Current card: #{@cur_card}   Discard pile top card: #{@discard_pile.last}"
+		puts "Current card: #{@cur_card} (#{@stock_pile.count} are left)  Discard pile top card: #{@discard_pile.last}"
 		if !@discard_pile.length.eql?(0) && card_type(@discard_pile.last) == 'K'
-			print "(M)ove pile, (P)lay card, (U)se discard, (D)iscard or (Q)uit ->"
+			print "(M)ove pile, (P)lay card, (U)se discard, (D)iscard or (Q)uit -> "
 		else
-			print "(M)ove pile, (P)lay card, (D)iscard or (Q)uit ->"
+			print "(M)ove pile, (P)lay card, (D)iscard or (Q)uit -> "
 		end
 		gets.chomp.upcase.slice(0)
 	end
@@ -233,8 +372,33 @@ class Klondike < Cards
 		puts ""
 	end
 
+	# def test_other_foundation_moves
+	# 	test_move_tableau_aces			# check for tableau Ace to move to foundation
+	# 	test_pile_to_pile							# check for a tableau pile to pile move
+	# 	test_pile_to_foundation					# check for a tableau card to foundation move
+	# end
+
+	# limit to two passes with same cards or game was won will stop the game
 	def gameover?
-		@stock_useless > 2		# two passes with same cards stop game
+		(@stock_useless.eql?(2) && (@stock_count.eql?(0) != @discard_pile.count.eql?(0)) && || gamewon?)
+	end
+
+	# limit to two passes with same cards or game was won will stop the game
+	def computer_gameover?
+		(@stock_useless > 2 || gamewon?)
+	end
+
+	# all cards used and the Foundation is full
+	def gamewon?
+		foundation_count.eql?(52)
+	end
+
+	def foundation_count
+		result = 0
+		@foundation.each do |k,v|
+			result += v.length
+		end
+		result
 	end
 
 	# card can go to foundation pile if No Cards exists on a pile and
@@ -242,6 +406,7 @@ class Klondike < Cards
 	# value is higher than the last card on the pile
 	def move_curcard_to_foundation
 		# No cards played for a suit and current card is an Ace
+		#puts "#{@cur_card}"
 		first_fndtn_card = @foundation[card_suit(@cur_card.strip)].first
 		if (first_fndtn_card.eql?('No Cards') && @cur_card[0].include?("A"))	
 			@foundation[card_suit(@cur_card.strip)] = [@cur_card]  
@@ -250,27 +415,25 @@ class Klondike < Cards
 			@foundation[card_suit(@cur_card.strip)] << @cur_card
 			set_card_was_played
 		else
-			puts "Card can't be used on the Foundation piles replay the card"
+			puts "Card can't be used on the Foundation piles replay the card" if verbose?
 		end
 	end
 
 	# card can go to foundation pile if No Cards exists on a pile and
 	# the card is an Ace or current card is the same suit and the 
 	# value is higher than the last card on the pile
-	def move_tableau_to_foundation
-		puts "Which pile (1-7)? "
-		p = gets.chomp.slice(0)
-		first_fndtn_card = @foundation[card_suit(@tableau[p.to_s+"U"].last)].first
-		last_tableau_card = @tableau[p.to_s+"U"].last
+	def move_tableau_to_foundation(pile)
+		last_tableau_card = @tableau[pile.to_s+"U"].last
+		first_fndtn_card = @foundation[card_suit(last_tableau_card)].first
 		tableau_pile_suit = card_suit(last_tableau_card)
 		if (first_fndtn_card.eql?('No Cards') && last_tableau_card.include?("A"))	# Ace
-			@foundation[tableau_pile_suit] = [@tableau[p.to_s+"U"].pop]  
+			@foundation[tableau_pile_suit] = [@tableau[pile.to_s+"U"].pop]  
 			check_tableau
 		elsif !first_fndtn_card.eql?('No Cards') && card_ok_for_foundation(last_tableau_card)
-			@foundation[tableau_pile_suit] << @tableau[p.to_s+"U"].pop
+			@foundation[tableau_pile_suit] << @tableau[pile.to_s+"U"].pop
 			check_tableau
 		else
-			puts "Card can't be used on the Foundation piles replay the card"
+			puts "Card can't be used on the Foundation piles replay the card" if verbose?
 		end
 	end
 
@@ -280,10 +443,14 @@ class Klondike < Cards
 		@cards_played += 1
 	end
 
+	def test_stock
+		flip_card if !current_card_exist?		# get new card from deck
+		reset_stock if stock_finished?					# test deck stock condition
+	end
+
 	# check the tableau piles to flip a down card if no cards are showing
 	def check_tableau
 		@tableau.each do |k,v| 
-#			if k.include? "U" && v.empty?
 			if !k.index("U").eql?(0) && v.empty? && !@tableau[k[0]+"D"].count.eql?(0)
 					@tableau[k] = [@tableau[k[0]+"D"].pop] #@tableau[k.sub("U","D")].pop 
 			end
@@ -293,7 +460,7 @@ class Klondike < Cards
 	# move current card to tableau pile 
 	def move_to_tableau(card, pile)
 		@tableau[pile.to_s+"U"] << card 	#@cur_card
-		puts "Card moved"
+		puts "Card moved" if verbose?
 	end
 
 	# move From tableau pile to To tableau pile
@@ -309,8 +476,9 @@ class Klondike < Cards
 	# OR moving a king to an empty pile
   def card_ok_for_tableau?(card, pile)	
 		(@tableau[pile.to_s+"U"].count.eql?(0) && card_type(card) == 'K' ) ||
-		 (card_value(card).next == ( card_value(@tableau[pile.to_s+"U"].last) ) &&
-		 !card_suit_color(card).eql?( card_suit_color(@tableau[pile.to_s+"U"].last) ) )
+			(@tableau[pile.to_s+"U"].last &&
+		 		(card_value(card).next == ( card_value(@tableau[pile.to_s+"U"].last) ) &&
+		 		!card_suit_color(card).eql?( card_suit_color(@tableau[pile.to_s+"U"].last) ) ) )
 	end
 
 	# pile can be move in tableau if 
@@ -320,11 +488,10 @@ class Klondike < Cards
   def ok_move_pile_to_pile_for_tableau?(from, to)	
   	tableau_from_pile = @tableau[from.to_s+"U"]
   	tableau_to_pile = @tableau[to.to_s+"U"]
-
 		return ( !tableau_from_pile.count.eql?(0) && !tableau_to_pile.count.eql?(0) &&
 						  (card_value(tableau_from_pile.first).next ==  card_value(tableau_to_pile.last))  &&
 		 					!card_suit_color(tableau_from_pile.first).eql?(card_suit_color(tableau_to_pile.last)) ) ||
-							( tableau_to_pile.count.eql?(0) && card_type(tableau_from_pile.first) == 'K' )
+							( tableau_to_pile.count.eql?(0) && card_type(tableau_from_pile.first) == 'K' && !tableau_from_pile.count.eql?(0))
 	end
 
 	# test current card against foundation pile to see if it is the same suit 
@@ -334,6 +501,7 @@ class Klondike < Cards
 	end
 
 end
+
 
 g = Klondike.new
 g.new_game
--- test.rb
+++ test.rb
@@ -1,1 +1,11 @@
-p 'Ace'.match(/#{Ace}+#{King}/)
+def slip
+	[]
+end
+
+t1 = []
+puts slip.eql?(t1)
+
+v1 = "A"
+v2 = v1
+v1 = 'B'
+puts "#{v2} #{v1}"
